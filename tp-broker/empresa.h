d45cc0e6c8e30aa8127b74e2acae6507718dbeac
Sofia Martinez <109770519+Samm2904@users.noreply.github.com>
2025-10-31 02:26:04 -0300

    Refactor empresa.h to include new functions and headers

--- tp-broker/empresa.h
+++ tp-broker/empresa.h
@@ -1,21 +1,27 @@
-#ifndef EMPRESA_H_INCLUDED
-#define EMPRESA_H_INCLUDED
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "inversion.h"
+#include "cliente.h"
+
 void menuEmpresa();
 
 typedef struct empresa{
     	char id_ticker[10];
-    	char nombre[50],password[20];
+    	char nombre[50];
     	float precio_actual;
 
 	} empresa;
 void verHistoricoOperaciones(empresa e, cliente listaClientes[], int cantidadClientes);
-empresa validarEmpresa(empresa e,char nombreEmpresaIngresado[]); ///MODIFICAR CON LA BD DE EMPRESAAS PRESENTEEEEEEEEEEEEEEEEEEEEEE
+empresa validarEmpresaNombre(empresa baseEmpresas[],char nombreEmpresaIng[], int cantidadEmpresas);
+empresa validarEmpresaTicker(empresa baseEmpresas[],char tickerEmpresaIng[], int cantidadEmpresas);
+void listaEmpresas(empresa baseEmpresas[], int cantidadEmpresas);
 
 #endif // EMPRESA_H_INCLUDED
+
+
+
+
 
 
 
