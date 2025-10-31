1e6603e6cca452d6c0a09ff0c74a46980e792200
Sofia Martinez <109770519+Samm2904@users.noreply.github.com>
2025-10-31 02:26:27 -0300

    Refactor empresa.c for clarity and functionality

--- tp-broker/empresa.c
+++ tp-broker/empresa.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "empresa.h"
 
+
 void menuEmpresa(){
 	printf("Bienvenido al menu empresa.\n\r");
 	printf("1- Ver historico de operaciones.\n\r");
@@ -12,36 +13,59 @@ void menuEmpresa(){
 	printf("Ingrese una opcion:\n\r");
 }
 
-void verHistoricoOperaciones(empresa e, cliente listaClientes[], int cantidadClientes){
+void verHistoricoOperaciones(empresa e, cliente baseClientes[], int cantidadClientes){
   int opEncontradas=0;
-
   for(int i=0;i<cantidadClientes;i++){
-	for(int j=0;j<listaClientes[i].num_inversiones;j++){
-    	if(strcmp(e.id_ticker, listaClientes[i].inversiones[j].id_ticker)==0){
-      	printf("\nOperación encontrada (Cliente %s):\n\r", listaClientes[i].nombre);
-      	printf("- Tipo: %s\n\r", listaClientes[i].inversiones[j].operacion);
-      	printf("- Cantidad: %d\n\r", listaClientes[i].inversiones[j].cantidad_acciones);
-      	printf("- Precio: $%.2f\n\r", listaClientes[i].inversiones[j].precio_compra);
-
+	for(int j=0;j<baseClientes[i].num_inversiones;j++){
+    	if(strcmp(e.id_ticker,baseClientes[i].inversiones[j].id_ticker)==0){
+      	printf("OperaciÃ³n encontrada (Cliente %s):\n\r", baseClientes[i].nombre);
+      	printf("- Tipo: %c\n\r", baseClientes[i].inversiones[j].operacion);
+      	printf("- Cantidad: %d\n\r", baseClientes[i].inversiones[j].cantidad_acciones);
+      	printf("- Precio: $%.2f\n\r", baseClientes[i].inversiones[j].precio_compra);
+      	printf("- Fecha: %s\n\r",baseClientes[i].inversiones[j].fecha);
     	opEncontradas++;
     	}
 	}
   }
-	if (operacionesEncontradas == 0) {
+	if (opEncontradas == 0) {
     	printf("No se encontraron operaciones para el ticker %s.\n\r", e.id_ticker);
  	}
-	}
 
-empresa validarEmpresa(empresa e,char nombreEmpresa[]){	///MODIFICAR CON LA BD DE EMPRESAAS PRESENTEEEEEEEEEEEEEEEEEEEEEE
-empresa encontrado;
-	strcpy(encontrado.nombre,"");
+}
 
-    	for(int i=0;i<"cantidadEmpresas";i++){
-        	if(strcmp(listaEmpresas[i].nombre,nombreEmpresaIngresado) == 0) {
-                	encontrado=listaEmpresa[i];
+void listaEmpresas(empresa baseEmpresas[], int cantidadEmpresas){
+    printf("| NOMBRE | ID_TICKER |  PRECIO\n\r");
+    for(int i=0;i<cantidadEmpresas;i++){
+        printf("%s: %s         $%.2f\n\r",baseEmpresas[i].nombre,baseEmpresas[i].id_ticker,baseEmpresas[i].precio_actual);
+    }
+}
+
+empresa validarEmpresaNombre(empresa baseEmpresas[],char nombreEmpresaIng[], int cantidadEmpresas){
+    empresa encontrado;
+    strcpy(encontrado.nombre,"");
+
+    	for(int i=0;i<cantidadEmpresas;i++){
+        	if(strcmp(baseEmpresas[i].nombre,nombreEmpresaIng) == 0) {
+                	encontrado=baseEmpresas[i];
                 	break;
                 	}
             	}
 
 	return encontrado;
 }
+
+empresa validarEmpresaTicker(empresa baseEmpresas[],char tickerEmpresaIng[], int cantidadEmpresas){
+    empresa encontrado;
+    strcpy(encontrado.id_ticker,"");
+
+    	for(int i=0;i<cantidadEmpresas;i++){
+        	if(strcmp(baseEmpresas[i].id_ticker,tickerEmpresaIng) == 0) {
+                	encontrado=baseEmpresas[i];
+                	break;
+                	}
+            	}
+
+	return encontrado;
+}
+
+
